28b5f43743adf6578be261e176683cc21409f725
ruksharrohan156-codex <ruksharrohan156@gmail.com>
2026-05-04 13:32:37 +0530

    Added setup.sh to folder

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+chmod +x kaif.py
+echo "alias kaif='python $(pwd)/kaif.py'" >> ~/.bashrc
+echo "alias kaif='python $(pwd)/kaif.py'" >> ~/.zshrc
+echo "-----------------------------------------------"
+echo "   INSTALLATION DONE - BY KAIF SHAIKH"
+echo "   Ab sirf 'kaif' likho aur tool chalao!"
+echo "-----------------------------------------------"
